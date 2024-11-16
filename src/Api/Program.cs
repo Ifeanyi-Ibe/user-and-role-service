09984b5c4a4b024d1b8924b1a6aa76af7d309b6f
Ibe Ifeanyi <ifeanyi-ibe@Ifeanyis-MacBook-Pro.local>
2024-11-16 10:15:38 +0100

    Initial commit

---
+++ src/Api/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Welcome to my world!")
+.WithName("Home")
+.WithOpenApi();
+
+app.Run();
